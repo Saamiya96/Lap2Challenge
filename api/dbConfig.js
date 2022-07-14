c7fe881adcbdf12ca0bafee572168cb86e723902
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-14 01:23:25 +0100

    trying again

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const { MongoClient } = require('mongodb')
 const connectionUrl = process.env.DB_CONNECTION;
 // const connectionUrl = process.env.MONGODB_URI
@@ -5,10 +6,24 @@ const connectionUrl = process.env.DB_CONNECTION;
 const dbName = process.env.DB_NAME
 
 const init = async () => {
-  let client = await MongoClient.connect(connectionUrl)
+  // let client = await MongoClient.connect(connectionUrl)
+  const client = new MongoClient(connectionUrl);
+  await client.connect()
   console.log('connected to database!', dbName)
   return client.db(dbName)
 }
 
 
 module.exports = { init };
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// module.exports = { init };
