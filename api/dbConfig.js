debca68360f9b81a16d96d42649104a80c39f56c
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-14 08:20:02 +0100

    changing dbconfig file

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -1,30 +1,29 @@
-// require("dotenv").config()
-// const { MongoClient } = require('mongodb')
-// // const connectionUrl = process.env.DB_CONNECTION;
-// const connectionUrl = process.env.MONGODB_URI 
+require("dotenv").config()
+const { MongoClient } = require('mongodb')
+// const connectionUrl = process.env.DB_CONNECTION;
+const connectionUrl = process.env.MONGODB_URI 
 
-// const dbName = process.env.DB_NAME
+const dbName = process.env.DB_NAME
 
-// const init = async () => {
-//   // let client = await MongoClient.connect(connectionUrl)
-//   const client = new MongoClient(connectionUrl);
-//   await client.connect()
-//   console.log('connected to database!', dbName)
-//   return client.db(dbName)
-// }
+const init = async () => {
+  // let client = await MongoClient.connect(connectionUrl)
+  const client = new MongoClient(connectionUrl);
+  await client.connect()
+  console.log('connected to database!', dbName)
+  return client.db(dbName)
+}
 
-
-// module.exports = { init };
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI || 'mongodb+srv://Saamiya96:074426Kitten@telegraphcluster.3s8jh.mongodb.net/?retryWrites=true&w=majority'
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 module.exports = { init };
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI || 'mongodb+srv://Saamiya96:074426Kitten@telegraphcluster.3s8jh.mongodb.net/?retryWrites=true&w=majority'
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// module.exports = { init };
